26bdb242e0cfde937871863a89d890ac2a6960d6
arnaud sellenet <arnodmental@gmail.com>
2014-05-12 17:07:26 +0200

    minify translation entity

--- lib/locomotive/entities/translation.rb
+++ lib/locomotive/entities/translation.rb
@@ -6,21 +6,7 @@ module Locomotive
 
       ## fields ##
       field :key
-      field :values
-
-      ## methods ##
-
-      def get(locale)
-        self.values[locale.to_s]
-      end
-
-      def to_params
-        { key: self.key, values: self.values }
-      end
-
-      def to_s
-        "Translation {#{self.key}: #{self.values}}"
-      end
+      field :values, localized: true
 
     end
   end
