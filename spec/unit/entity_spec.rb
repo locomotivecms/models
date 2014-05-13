c7cde53fb4cb0ff33d50320ce4f41c805fbf0a6e
arnaud sellenet <arnodmental@gmail.com>
2014-05-13 18:03:01 +0200

    entity back to its simplest usage

--- spec/unit/entity_spec.rb
+++ spec/unit/entity_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe Locomotive::Entity do
-  let(:attributes) {{ mounting_point: nil, name: 'bar' }}
-
-  class FakeEntity < Locomotive::Entity
-    field :name
+  
+  class FakeEntity
+    include Locomotive::Entity
+    attributes :name, :description
   end
 
-  subject { FakeEntity.new(attributes) }
-
-  describe '#to_record' do
-    it 'returns an entity' do
-      subject.to_record.should eq({name: 'bar'})
-    end
+  context 'simplest instanciation' do
+    subject { FakeEntity.new }
+    it { should be_a FakeEntity }
+    it { should respond_to :name }
+    it { should respond_to :name= }    
   end
+
+  context 'with empty hash' do
+    subject { FakeEntity.new({}) }
+    it { should be_a FakeEntity }
+    its(:name) { should be_nil }
+  end
+
+  context 'with hash and data in it' do
+    subject { FakeEntity.new({name: 'John'}) }
+    it { should be_a FakeEntity }
+    its(:name) { should eq 'John' }
+    its(:description) { should be_nil }    
+  end
+
 end
