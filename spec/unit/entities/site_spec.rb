e68fe1cb709fdefb1862f4e0582903a84e0a8372
arnaud sellenet <arnodmental@gmail.com>
2014-05-16 09:33:37 +0200

    site#to_s

--- spec/unit/entities/site_spec.rb
+++ spec/unit/entities/site_spec.rb
@@ -6,6 +6,12 @@ describe Locomotive::Entities::Site do
     build_site.should_not be_nil
   end
 
+  describe '#to_s' do
+    it 'returns the site name' do
+      build_site(name: 'My Site').to_s.should eq 'My Site'
+    end
+  end
+
   def build_site(attributes = {})
     Locomotive::Entities::Site.new(attributes)
   end
