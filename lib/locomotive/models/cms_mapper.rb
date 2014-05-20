739da6b0453dc29c1f5372e06a3f955f4f6de135
arnaud sellenet <arnodmental@gmail.com>
2014-05-20 19:26:30 +0200

    prepare mapper with all collections

--- lib/locomotive/models/cms_mapper.rb
+++ lib/locomotive/models/cms_mapper.rb
@@ -1,3 +1,39 @@
 collection :site do
 
 end
+
+collection :page do
+
+end
+
+collection :content_type do
+
+end
+
+collection :content_entry do
+
+end
+
+collection :content_field do
+
+end
+
+collection :content_select_option do
+
+end
+
+collection :editable_element do
+
+end
+
+collection :snippet do
+
+end
+
+collection :theme_asset do
+
+end
+
+collection :translation do
+
+end
