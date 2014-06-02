be117f8ca0a80349ec43b092b233d8d6fc0a20c5
Joel AZEMAR <joel.azemar@gmail.com>
2014-06-02 11:48:51 +0200

    add sample for non persisted object

---
+++ non_persisted_example.rb
@@ -0,0 +1,76 @@
+# irb -r locomotive/models -I ./lib
+
+require 'pry'
+
+adapter = Locomotive::Adapters::MemoryAdapter
+locale = :en
+
+class Article
+  include Locomotive::Entity
+  attributes :title, :author, :comments
+end
+
+class Author
+  include Locomotive::Entity
+  attributes :name
+end
+
+class Comment
+  include Locomotive::Entity
+  attributes :title
+end
+
+class ArticlesRepository
+  include Locomotive::Repository
+end
+
+class AuthorsRepository
+  include Locomotive::Repository
+end
+
+class CommentsRepository
+  include Locomotive::Repository
+
+  def where(locale, constraints, values)
+    query(locale) do
+      where(constraints => values)
+    end
+  end
+end
+
+mapper = Locomotive::Mapper.new(adapter) do
+  collection :articles do
+    entity Article
+    repository ArticlesRepository
+
+    attribute :title
+    attribute :author, association: :authors
+    attribute :comments, association: :comments
+  end
+
+  collection :authors do
+    entity Author
+    repository AuthorsRepository
+
+    attribute :name
+  end
+
+  collection :comments do
+    entity Comment
+    repository CommentsRepository
+
+    attribute :title
+  end
+
+end.load!
+
+articles_repository = Locomotive::Models[:articles]
+
+author  = Author.new(name: 'John')
+comment = Comment.new(title: 'New Comment')
+article = Article.new(title: "Title #{rand(100_000)}", author: author, comments: [comment])
+articles_repository.create article, locale
+
+my_article = articles_repository.find author.id, locale
+my_article.author
+my_article.comments
