f5fa14670115f8818be30b9ede26e39eda46411c
arnaud sellenet <arnodmental@gmail.com>
2014-06-10 12:11:32 +0200

    i18n decorator, allowing to choose locale in which the object will be rendered

---
+++ lib/locomotive/models/decorators/i18n_decorator.rb
@@ -0,0 +1,22 @@
+module Locomotive
+  module Decorators
+    class I18nDecorator < SimpleDelegator
+
+      attr_accessor :current_locale
+      class << self
+        def decorate(resultset, locale = nil)
+          resultset.map { |obj| new(obj).tap {|decorated_obj| decorated_obj.current_locale = locale} }
+        end
+      end
+
+      def method_missing(name, *args, &block)
+        begin
+          __getobj__.public_send(name)[current_locale]
+        rescue TypeError
+          super
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/unit/decorators/i18n_decorator_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+require 'locomotive/models/decorators'
+
+describe Locomotive::Decorators::I18nDecorator, focused: true do
+
+  class FakeEntity
+    include Locomotive::Entity
+    attributes :name, :description
+  end
+
+  describe 'initialization' do
+    context 'with instance' do
+      subject do
+        Locomotive::Decorators::I18nDecorator.new(FakeEntity.new)
+      end
+
+      it 'accepts an entity as parameter' do
+        subject.should be_kind_of Locomotive::Decorators::I18nDecorator
+      end
+
+      it 'responds to all entity attributes' do
+        subject.should respond_to :name
+        subject.should respond_to :description
+        subject.should respond_to :name=
+        subject.should respond_to :description=
+      end
+    end
+
+    context 'with resultset' do
+      let(:entities) {
+        [ FakeEntity.new, FakeEntity.new ]
+      }
+      it 'decorates each entity' do
+        Locomotive::Decorators::I18nDecorator.decorate(entities).each do |entity|
+          entity.should be_kind_of(Locomotive::Decorators::I18nDecorator)
+        end
+      end
+    end
+  end
+
+  describe 'fields on single instance' do
+
+    subject { decorator.name.to_s }
+    let(:decorator) { Locomotive::Decorators::I18nDecorator.new(entity) }
+    let(:entity) { FakeEntity.new(name: name) }
+
+    context 'when field is not an i18n field' do
+      let(:name) { 'blah' }
+      it { should eql 'blah' }
+    end
+
+    context 'when field is an i18n field' do
+      let(:name) { { en: 'blah', fr: 'bla' } }
+      context 'no current locale' do
+        # TODO redundant test here ( @see fields/i18n_field_spec.rb )
+        it 'falls back to default locale' do
+          subject.should eql 'blah'
+        end
+      end
+
+      context 'with current locale' do
+        before { decorator.current_locale = :fr }
+        it { should eql 'bla' }
+      end
+    end
+  end
+
+  describe 'fields on resultset' do
+    subject { decorated_set.first.name.to_s }
+    let(:entities) {
+      [ FakeEntity.new(name: { en: 'blah', fr: 'bla'}),
+        FakeEntity.new
+      ]
+    }
+    context 'when no block is given' do
+      let(:decorated_set) do
+        Locomotive::Decorators::I18nDecorator.decorate(entities)
+      end
+      it { should eq 'blah' }
+    end
+    context 'when locale is set in a given block' do
+      let(:decorated_set) do
+        Locomotive::Decorators::I18nDecorator.decorate(entities, :fr)
+      end
+      it { should eq 'bla' }
+    end
+  end
+end
